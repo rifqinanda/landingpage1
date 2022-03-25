84328cf641ae524a7986f11c4613b74e96d07e7b
rifqinanda <rifqinanda51@gmail.com>
2022-03-25 07:59:40 +0700

    first

---
+++ resources/views/frontend/landingpage/index.blade.php
@@ -0,0 +1,236 @@
+@extends('layout')
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Landingpage</title>
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-light navbar-expand-lg shadow p-3">
+		<div class="container">
+			<div class="navbar-brand">
+				<a href="/"><img src="/assets/img/logo_sleepy.png" class="img-fluid"></a>
+			</div>
+			<button class="navbar-toggler ms-auto" data-bs-target="#navbarNav" data-bs-toggle="collapse">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav ms-auto">
+					<li class="nav-item"><a href="#" class="nav-link h5 text-dark fw-bold">About</a></li>
+					<li class="nav-item"><a href="#" class="nav-link h5 text-dark fw-bold">Blog</a></li>
+					<li class="nav-item"><a href="#" class="nav-link h5 text-dark fw-bold">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
+<section class="container">
+	<div class="py-5">
+		<div class="row flex-column-reverse flex-md-row">
+			<div class="col-12 col-md-5 py-2 my-auto">
+				<h1 class="h1 fw-bold text-color1">Vacation with Sleepy</h1>
+				<h1 class="h1 fw-bold">Lorem ipsum dolor sit amet</h1>
+				<div class="d-md-none d-block d-lg-block my-3">
+					<button class="btn btn-primary">Getting Start</button>
+				</div>
+			</div>
+			<div class="col-12 col-md-7">
+				<img src="/assets/img/img_hero_section.png" class="img-fluid">
+			</div>
+		</div>
+		<div class="d-md-block d-none d-lg-none my-3 text-center">
+			<button class="btn btn-primary">Getting Start</button>
+		</div>
+	</div>
+</section>
+<section class="py-5">
+	<div class="container">
+		<div class="text-center">
+			<h1 class="h1 fw-bold">Vacation with Sleepy</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+		</div>
+		<div class="row py-3">
+			<div class="col-12 col-md-4 py-2">
+				<h3 class="h3 fw-bold text-color1">Fast</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+				<div class="text-center bg-color1 bg-1 bg-2 py-3 px-3">
+					<img src="/assets/img/img_fast.png" class="img-fluid">
+				</div>
+			</div>
+			<div class="col-12 col-md-4 py-2">
+				<h3 class="h3 fw-bold text-color1">Accurate</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+				<div class="text-center bg-color1 bg-1 bg-2 py-3 px-3">
+					<img src="/assets/img/img_accurate.png" class="img-fluid">
+				</div>
+			</div>
+			<div class="col-12 col-md-4 py-2">
+				<h3 class="h3 fw-bold text-color1">Trusted</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+				<div class="text-center bg-color1 bg-1 bg-2 py-3 px-3">
+					<img src="/assets/img/img_trusted.png" class="img-fluid">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="bg-color2">
+	<div class="py-5">
+		<div class="container">
+			<div class="row flex-md-column-reverse flex-lg-row">
+				<div class="col-12 col-lg-6">
+					<img src="/assets/img/img_banner_start_first_order_01.png" class="img-fluid">
+					<div class="row py-3">
+						<div class="col-4">
+							<img src="/assets/img/img_banner_start_first_order_02.png" class="img-fluid">
+						</div>
+						<div class="col-4">
+							<img src="/assets/img/img_banner_start_first_order_03.png" class="img-fluid">
+						</div>
+						<div class="col-4">
+							<img src="/assets/img/img_banner_start_first_order_04.png" class="img-fluid">
+						</div>
+					</div>
+					<div class="text-center py-2 d-md-block d-none d-lg-none">
+						<button class="btn btn-light">Get Started</button>
+					</div>
+				</div>
+				<div class="col-12 col-lg-6">
+					<div class="text-md-center text-start text-lg-start">
+						<h1 class="h1 fw-bold text-light">Start Your First Order With Sleepy</h1>
+						<p class="text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.</p>
+						<div class="py-2 d-md-none d-block d-lg-block">
+							<button class="btn btn-light">Get Started</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="my-5">
+	<div class="container">
+		<div class="text-center">
+			<h1 class="h1 fw-bold">Hot Destination</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+		<div class="row py-3">
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/assets/img/img_hot_destination_01.png" class="img-fluid w-100">
+				<h3 class="h3 fw-bold pt-2">Verstibucu Arcu</h3>
+				<div>
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star.png" class="img-fluid">
+				</div>
+				<div class="py-3">
+					<button class="btn btn-outline-dark">Bali</button>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/assets/img/img_hot_destination_02.png" class="img-fluid w-100">
+				<h3 class="h3 fw-bold pt-2">Verstibucu Arcu</h3>
+				<div>
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star.png" class="img-fluid">
+				</div>
+				<div class="py-3">
+					<button class="btn btn-outline-dark">Bali</button>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/assets/img/img_hot_destination_03.png" class="img-fluid w-100">
+				<h3 class="h3 fw-bold pt-2">Verstibucu Arcu</h3>
+				<div>
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star.png" class="img-fluid">
+				</div>
+				<div class="py-3">
+					<button class="btn btn-outline-dark">Bali</button>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-3">
+				<img src="/assets/img/img_hot_destination_04.png" class="img-fluid w-100">
+				<h3 class="h3 fw-bold pt-2">Verstibucu Arcu</h3>
+				<div>
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star_filled.png" class="img-fluid">
+					<img src="/assets/img/img_star.png" class="img-fluid">
+				</div>
+				<div class="py-3">
+					<button class="btn btn-outline-dark">Bali</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="bg-color1">
+	<div class="py-5">
+		<div class="container">
+			<div class="row flex-column flex-lg-row">
+				<div class="col-12 col-lg-6">
+					<div class="text-center text-lg-start">
+						<h1 class="h1 fw-bold">Get In Touch</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+				</div>
+				<div class="col-12 col-lg-6">
+					<div class="row text-center text-lg-start">
+						<div class="col-4">
+							<img src="/assets/img/img_fb.png" class="img-fluid">
+						</div>
+						<div class="col-4">
+							<img src="/assets/img/img_twitter.png" class="img-fluid">
+						</div>
+						<div class="col-4">
+							<img src="/assets/img/img_wa.png" class="img-fluid">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<footer class="bg-color2">
+	<div class="py-5">
+		<div class="container">
+			<div class="row flex-column flex-lg-row">
+				<div class="col-12 col-lg-6 text-center text-lg-start">
+					<img src="/assets/img/logo_sleepy_white.png" class="img-fluid">
+				</div>
+				<div class="col-12 col-lg-6 text-center text-lg-start d-lg-flex">
+					<div class="">
+						<img src="/assets/img/img_location.png" class="img-fluid">
+						<p class="text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</div>
+					<div class="">
+						<img src="/assets/img/img_phone.png" class="img-fluid">
+						<p class="text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+</head>
+<body>
+<script type="text/javascript" src="/assets/js/bootstrap.js"></script>
+</body>
+</html>
